27dbde6b77cd545a96380578ca753b2b9849d9a2
alex-1900 <omytty@126.com>
2022-09-11 15:54:45 +0800

    update

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import type { HeadFC } from "gatsby"
+
+
+export default function IndexPage() {
+  return (
+      <main>
+      </main>
+  )
+}
+
+export const Head: HeadFC = () => <title>Home Page</title>
